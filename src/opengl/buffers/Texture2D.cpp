6116610d7083140d4d13aaef2165ac5a7b4554c4
Mark <kugelflash@gmail.com>
2023-01-30 00:36:52 +0100

    Added Filter to Texture2D

--- src/opengl/buffers/Texture2D.cpp
+++ src/opengl/buffers/Texture2D.cpp
@@ -2,13 +2,13 @@
 
 namespace Kikan {
 
-    Texture2D::Texture2D(GLsizei width, GLsizei height, float data[]) : _width(width), _height(height){
-        gen();
+    Texture2D::Texture2D(GLsizei width, GLsizei height, float data[], GLenum filter) : _width(width), _height(height){
+        gen(filter);
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, _width, _height, 0, GL_RGBA, GL_FLOAT, data);
         unbind();
     }
-    Texture2D::Texture2D(GLsizei width, GLsizei height, unsigned char data[]) : _width(width), _height(height){
-        gen();
+    Texture2D::Texture2D(GLsizei width, GLsizei height, unsigned char data[], GLenum filter) : _width(width), _height(height){
+        gen(filter);
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, _width, _height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
         unbind();
     }
@@ -30,12 +30,12 @@ namespace Kikan {
         return _id;
     }
 
-    void Texture2D::gen() {
+    void Texture2D::gen(GLenum filter) {
         glGenTextures(1, &_id);
         bind(0);
 
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, filter);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, filter);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
     }
