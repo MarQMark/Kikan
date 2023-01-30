1ba008d372e7057431fdc4a8b775f69d8aa30318
Mark <kugelflash@gmail.com>
2023-01-30 21:52:14 +0100

    Added Texture Setter

--- src/opengl/buffers/Texture2D.cpp
+++ src/opengl/buffers/Texture2D.cpp
@@ -32,11 +32,29 @@ namespace Kikan {
 
     void Texture2D::gen(GLenum filter) {
         glGenTextures(1, &_id);
-        bind(0);
+        bind();
 
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, filter);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, filter);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    }
+
+    void Texture2D::set(GLsizei width, GLsizei height, float *data) {
+        _width = width;
+        _height = height;
+
+        bind();
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, _width, _height, 0, GL_RGBA, GL_FLOAT, data);
+        unbind();
+    }
+
+    void Texture2D::set(GLsizei width, GLsizei height, unsigned char *data) {
+        _width = width;
+        _height = height;
+
+        bind();
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, _width, _height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        unbind();
     }
 }
